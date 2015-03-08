04afcba38e07294062f28552deead31e3f35e8c6
aleib <aleib.start@gmail.com>
2015-03-08 09:32:00 +0200

    Working through tutorial

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'tutorial')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
